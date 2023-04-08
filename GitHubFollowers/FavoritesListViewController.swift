9ab9e7741225af15757b0dfad13f817b6efc8728
Carlos Zinato <carlosh.lz@hotmail.com>
2023-04-08 14:16:46 +0200

    initial commit

--- GitHubFollowers/FavoritesListViewController.swift
+++ GitHubFollowers/FavoritesListViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListViewController.swift
 //  GitHubFollowers
 //
 //  Created by Carlos Zinato on 14/02/2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
     }
-
-
 }
-
---
+++ GitHubFollowers/GFTextField.swift
@@ -0,0 +1,41 @@
+//
+//  GFTextField.swift
+//  GitHubFollowers
+//
+//  Created by Carlos Zinato on 14/02/2022.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label // label as color is basically black for white theme and white for dark theme
+        tintColor = .label // blinking cursor
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true // font shrinks if text is too long
+        minimumFontSize = 12 // doesn't shrink too much
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        
+        placeholder = "Enter a username"
+    }
+    
+}
